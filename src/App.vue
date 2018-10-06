54f07287df67f11be2d8e492c9acf2d8c3d0ea98
Ahmed Tammam <tamam65@gmail.com>
2018-10-06 03:34:55 +0200

    plugin run

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,58 @@
 <template>
-  <div>
-  <medium-editor :text='text' v-on:edit='processEditOperation' custom-tag='div'>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <medium-editor :text='text' :options="options" :toolbar="toolbar" v-on:edit='processEditOperation' custom-tag='textarea'>
 </medium-editor>
+      </div>
 </div>
+  </div>
 </template>
 
 <script>
 import editor from 'vue2-medium-editor'
 
-
 export default {
+  
+
   name: 'app',
   components: {
     'medium-editor': editor
   },
   data: () => ({
-    text: 'text'
+    text: '',
+    options: {
+    activeButtonClass: 'medium-editor-button-active',
+    buttonLabels: false,
+    contentWindow: window,
+    delay: 0,
+    disableReturn: false,
+    disableDoubleReturn: false,
+    disableExtraSpaces: false,
+    disableEditing: false,
+    elementsContainer: false,
+    extensions: {},
+    ownerDocument: document,
+    spellcheck: true,
+    targetBlank: false
+    },
+    toolbar: {
+        /* These are the default options for the toolbar,
+           if nothing is passed this is what is used */
+        buttons: ['bold', 'italic', 'underline', 'anchor', 'h2', 'h3'],
+        diffLeft: 0,
+        diffTop: -10,
+        firstButtonClass: 'medium-editor-button-first',
+        lastButtonClass: 'medium-editor-button-last',
+        relativeContainer: null,
+        standardizeSelectionStart: false,
+        static: true,
+
+        /* options which only apply when static is true */
+        align: 'center',
+        sticky: false,
+        updateOnEmptySelection: false
+    }
   }),
   methods: {
     processEditOperation: function (operation) {
@@ -26,4 +63,5 @@ export default {
 </script>
 
 <style>
+@import './assets/css/themes/bootstrap.css';
 </style>
